705a0330499316672b9a909fbf785a563718a9bf
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-02-10 22:59:26 -0600

    Refactoring Example of a Bank Class

---
+++ src/ModularSoftwareDesign/BankExample/BankProgram.java
@@ -0,0 +1,8 @@
+package ModularSoftwareDesign.BankExample;
+
+public class BankProgram {
+    public static void main(String[] args) {
+        BankClient client = new BankClient();
+        client.run();
+    }
+}
